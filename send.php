949a9c3b7b89b7253bcec9cd5239b205f741534a
Ishmenev <ishmenevev@mail.ru>
2020-09-01 05:40:31 +0300

    php fixed

--- send.php
+++ send.php
@@ -1,70 +1,64 @@
 <?php
-// Файлы phpmailer
+
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-// Переменные, которые отправляет пользователь
-$phone = $_POST['phone'];
 $email = $_POST['email'];
+$phone = $_POST['phone'];
 $comment = $_POST['comment'];
-// $file = $_FILES['myfile'];
 
-// Формирование самого письма
-$title = "Заголовок письма";
+
+// шаблон письма
+$title = "Обратная связь:";
 $body = "
-<h2>Тестовое письмо</h2>
+<h2>Новое письмо</h2>
 <b>Телефон:</b> $phone<br>
 <b>Почта:</b> $email<br><br>
 <b>Сообщение:</b><br>$comment
 ";
 
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
-    $mail->isSMTP();   
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
-    //$mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+// Валидация почты
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
-    $mail->Username   = 'ishmenevev'; // Логин на почте
-    $mail->Password   = 'test-mailer'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('ishmenevev@yandex.ru', 'Евгений'); // Адрес самой почты и имя отправителя
+    // конфиг PHPMailer
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    try {
+        $mail->isSMTP();   
+        $mail->CharSet = "UTF-8";
+        $mail->SMTPAuth   = true;
+        //$mail->SMTPDebug = 2;
+        $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
-    // Получатель письма
-    $mail->addAddress('ishmenevev@mail.ru');  
+        $mail->Host       = 'smtp.yandex.ru'; 
+        $mail->Username   = 'ishmenevev'; 
+        $mail->Password   = 'test-mailer'; 
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port       = 465;
+        $mail->setFrom('ishmenevev@yandex.ru', 'Евгений');
 
-    // Возможность прикрепить файлы к письму
-if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-        $filename = $file['name'][$ct];
-        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-            $mail->addAttachment($uploadfile, $filename);
-            $rfile[] = "Файл $filename прикреплён";
+        // Получатель письма
+        $mail->addAddress('ishmenevev@mail.ru');  
+
+        $mail->isHTML(true);
+        $mail->Subject = $title;
+        $mail->Body = $body;    
+
+        // проверка при отправлении
+        if ($mail->send()) {
+            $result = "success";
         } else {
-            $rfile[] = "Не удалось прикрепить файл $filename";
+            $result = "error";
         }
-    }   
+
+    } catch (Exception $e) {
+        $result = "error";
+        $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+    }
+} else {
+	$result = "email";
 }
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
-
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";} 
-else {$result = "error";}
-
-} catch (Exception $e) {
-    $result = "error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
-}
-
-// Отображение результата
+// общий результат
 echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+
+?>
